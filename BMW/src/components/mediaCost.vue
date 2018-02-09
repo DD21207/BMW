6f81fa9c55181db8a2fb734230e52e0b3ceb635f
DD21207 <1005745397@qq.com>
2018-02-09 18:10:52 +0800

    20180209

--- BMW/src/components/mediaCost.vue
+++ BMW/src/components/mediaCost.vue
@@ -1,24 +1,36 @@
 <template>
-	<div style="height:100%">
+	<div style="height:100%" :selected="selected">
 		xxxxx
 	</div>
 </template>
 
 <script>
 export default {
-	props: {
-	    // id: {
-	    //   type: String
-	    // },
-	    // data: {
-	      
-	    // }
-  	},
+		props:['selected'],
 	name:"mediaCost",
 	data(){
 		return {
-
+			selectedData:this.selected
 		}
+	},
+	mounted(){
+		$("#Audit").css('background','#262626')
+		// this.$nextTick(function() {
+  // 	 		this.competitiveOnload()	   
+  //   	})
+  		// console.log(this.selectedData)
+	},
+	methods:{
+
+		mediaCostLoad(){
+			// alert("sdsd")
+		}
+	},
+	watch:{
+		'selectedData.valueBrand':'mediaCostLoad',
+		'selectedData.monthStart':'mediaCostLoad',
+		'selectedData.monthEnd':'mediaCostLoad',
+		'selectedData.valueYear':'mediaCostLoad',
 	}
 }	
 </script>
